4ca38ea2ecae18966fddd669177e44a8a2eed732
Wex <bernikcooldude@yandex.ru>
2022-10-03 23:07:07 +0300

    fraud pattern: менее минуты между зачислением средств и их списанием

--- src/main/java/frauds/Fraud.java
+++ src/main/java/frauds/Fraud.java
@@ -1,7 +1,6 @@
 package frauds;
 
 import java.sql.SQLException;
-import java.util.HashMap;
 import java.util.HashSet;
 
 public interface Fraud {
--- src/main/resources/testQuerries.sql
+++ src/main/resources/testQuerries.sql
@@ -1,5 +1,4 @@
-SELECT transactions.id, amount, oper_type, oper_result, account, account_valid_to, terminal, city FROM users INNER JOIN
-    transactions ON(users.transaction_id = transactions.id) WHERE transaction_date::text LIKE '2020-05-01 __:__:__' AND client = '6-13938';
+SELECT oper_type, transaction_date, transaction_id FROM users INNER JOIN transactions ON(users.transaction_id = transactions.id) WHERE client = '3-95179' AND oper_type IN('Пополнение', 'Снятие') ORDER BY transaction_date;
 
 
 SELECT transactions.id FROM users INNER JOIN
